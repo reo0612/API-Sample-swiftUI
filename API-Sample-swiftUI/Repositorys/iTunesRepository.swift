6bb2c0e7aaf89f9f343b55879558c1f55fac3631
今西レオ <imanishi8033@gmail.com>
2021-05-31 10:22:59 +0900

    Repositoryを構築しました

--- API-Sample-swiftUI/Repositorys/iTunesRepository.swift
+++ API-Sample-swiftUI/Repositorys/iTunesRepository.swift
@@ -2,4 +2,12 @@
 import Foundation
 import Combine
 
+// MVVMでのRepositoryの役目として処理した結果をViewModelへ返す役割をしている
+// この仕組みによって、ViewModelは与えられたデータが何の値かを意識せずにViewの状態管理に専念できる
 
+// APIClientsを呼び出して、値を取得する
+struct iTunesRepository {
+    func fetchiTunes() -> AnyPublisher<iTunesModel, Error> {
+        return iTunesAPIClient().get()
+    }
+}
