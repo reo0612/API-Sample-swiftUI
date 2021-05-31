00d8833af05901e17e475c5e4e6ed5136efe4108
今西レオ <imanishi8033@gmail.com>
2021-05-31 10:34:08 +0900

    追加しました ListのRowを構築するため

--- API-Sample-swiftUI/View/ListRow.swift
+++ API-Sample-swiftUI/View/ListRow.swift
@@ -2,3 +2,19 @@
 import SwiftUI
 import SDWebImageSwiftUI
 
+struct ListRow: View {
+    let result: Result
+    
+    var body: some View {
+        HStack {
+            WebImage(url: URL(string: result.artworkUrl60 ?? ""))
+            
+            VStack(alignment: .leading) {
+                Text(result.artistName ?? "No ArtistName")
+                    .bold()
+                Text(result.trackName ?? "No TrackName")
+                    .italic()
+            }
+        }
+    }
+}
